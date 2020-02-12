287e4c7c6717e551c3fd67ce8dccffbb47b5ddaa
Travis CI User <travis@example.org>
2020-02-12 11:01:16 +0000

    Update documentation

---
+++ aliases.js
@@ -0,0 +1,6 @@
+var ALIASES = {};
+ALIASES["byteorder"] = {};
+ALIASES["bytes"] = {};
+ALIASES["eui48"] = {};
+ALIASES["ieee80211"] = {};
+ALIASES["rustc_serialize"] = {};
---
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["eui48"] = [{"text":"impl <a class=\"trait\" href=\"rustc_serialize/serialize/trait.Encodable.html\" title=\"trait rustc_serialize::serialize::Encodable\">Encodable</a> for <a class=\"struct\" href=\"eui48/struct.MacAddress.html\" title=\"struct eui48::MacAddress\">MacAddress</a>","synthetic":false,"types":["eui48::MacAddress"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
